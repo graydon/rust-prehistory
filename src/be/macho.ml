2dec7c6a645b49b9a825a4c79f543bc31ff7c848
Graydon Hoare <graydon@pobox.com>
2009-04-24 20:08:40 -0700

    A little mach-o stuff.

--- src/be/macho.ml
+++ src/be/macho.ml
@@ -2,20 +2,83 @@ open Asm;;
 open Common;;
 
 
-type cpu_type = 
+let (cpu_arch_abi64:int64) = 0x01000000L
+;;
+
+let (mh_magic:int64) = 0xfeedfaceL
+;;
+
+let cpu_subtype_intel (f:int64) (m:int64) : int64 =
+  Int64.add f (Int64.shift_left m 4)
+;;
+
+type cpu_type =
     (* Maybe support more later. *)
-    CPU_TYPE_X86 
+    CPU_TYPE_X86
   | CPU_TYPE_X86_64
+  | CPU_TYPE_ARM
+  | CPU_TYPE_POWERPC
+;;
+
+type cpu_subtype =
+    (* Maybe support more later. *)
+    CPU_SUBTYPE_X86_ALL
+  | CPU_SUBTYPE_X86_64_ALL
+  | CPU_SUBTYPE_ARM_ALL
+  | CPU_SUBTYPE_POWERPC_ALL
+;;
+
+type file_type =
+    MH_OBJECT
+  | MH_EXECUTE
+  | MH_FVMLIB
+  | MH_CORE
+  | MH_PRELOAD
+  | MH_DYLIB
+  | MH_DYLINKER
+  | MH_BUNDLE
+  | MH_DYLIB_STUB
+  | MH_DSYM
+;;
+
+let file_type_code (ft:file_type) : int64 =
+  match ft with
+      MH_OBJECT ->0x1L      (* object *)
+    | MH_EXECUTE -> 0x2L    (* executable *)
+    | MH_FVMLIB -> 0x3L     (* fixed-VM shared lib *)
+    | MH_CORE -> 0x4L       (* core *)
+    | MH_PRELOAD -> 0x5L    (* preloaded executable *)
+    | MH_DYLIB -> 0x6L      (* dynamic lib *)
+    | MH_DYLINKER -> 0x7L   (* dynamic linker *)
+    | MH_BUNDLE -> 0x8L     (* bundle *)
+    | MH_DYLIB_STUB -> 0x9L (* shared lib stub *)
+    | MH_DSYM -> 0xaL       (* debuginfo only *)
 ;;
 
 
-let macho_header (cpu:cpu_type) = 
-  SEQ 
+let cpu_type_code (cpu:cpu_type) : int64 =
+  match cpu with
+      CPU_TYPE_X86 -> 7L
+    | CPU_TYPE_X86_64 -> Int64.logor 7L cpu_arch_abi64
+    | CPU_TYPE_ARM -> 12L
+    | CPU_TYPE_POWERPC -> 18L
+;;
+
+let cpu_subtype_code (cpu:cpu_subtype) : int64 =
+  match cpu with
+      CPU_SUBTYPE_X86_ALL -> 3L
+    | CPU_SUBTYPE_X86_64_ALL -> 3L
+    | CPU_SUBTYPE_ARM_ALL -> 0L
+    | CPU_SUBTYPE_POWERPC_ALL -> 0L
+;;
+
+
+let macho_header_32 (cpu:cpu_type) (sub:cpu_subtype) =
+  SEQ
     [|
-      WORD (TY_u32, IMM (0xfeedfaceL));
-      WORD (TY_u32, (IMM (match cpu with
-                              CPU_TYPE_X86 -> 7L
-                            | CPU_TYPE_X86_64 -> 0x01000007L)));
+      WORD (TY_u32, IMM mh_magic);
+      WORD (TY_u32, IMM (cpu_type_code cpu));
+      WORD (TY_u32, IMM (cpu_subtype_code sub));
     |]
 
 let emit_file
@@ -27,7 +90,7 @@ let emit_file
     (c_to_proc_fixup:fixup)
     : unit =
   let
-      all_items = SEQ [| macho_header CPU_TYPE_X86 |]
+      all_items = SEQ [| macho_header_32 CPU_TYPE_X86 CPU_SUBTYPE_X86_ALL |]
   in
   let buf = Buffer.create 16 in
   let out = open_out_bin sess.Session.sess_out in
