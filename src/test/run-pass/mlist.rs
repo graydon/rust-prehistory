2b6f0afea53c064667cd644f33ca8534cccd3fb9
Graydon Hoare <graydon@mozilla.com>
2009-12-17 17:34:52 -0800

    Make mlist test simpler again.

--- src/test/run-pass/mlist.rs
+++ src/test/run-pass/mlist.rs
@@ -2,18 +2,14 @@
 
 prog a
 {
-  type visual = tag(shade(int),hue(pix));
-  type pix = (colour,colour,colour);
-  type colour = tag(red(),green(),blue());
-  type mlist = tag(cons(int,@next), nil());
-  type next = rec(mlist val);
+  type mlist = tag(cons(int,@mlist), nil());
 
   main {
     // FIXME: at the moment we can't handle x = conx(10, x)
     // because it'll leak x._1 during the initializing-call.
     // Yuck!
     auto x = nil();
-    auto x1 = cons(10, rec(val=x));
-    auto x2 = cons(10, rec(val=cons(11, rec(val=cons(12, rec(val=x1))))));
+    auto x1 = cons(10, x);
+    auto x2 = cons(10, cons(11, cons(12, x1)));
   }
 }
