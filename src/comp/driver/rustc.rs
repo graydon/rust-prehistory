a1366f1103140c509be1c2abbcca0f286b0505cf
Graydon Hoare <graydon@mozilla.com>
2010-01-07 17:12:30 -0800

    Fix up rustc, oops.

--- src/comp/driver/rustc.rs
+++ src/comp/driver/rustc.rs
@@ -1,24 +1,17 @@
 // -*- C -*-
 
-prog rustc
-{
-  let str filename;
-  init () -> () {
-    filename = "comp/rustc.rc";
+fn main() -> () {
+  let str filename = "comp/rustc.rc";
+  puts(str_buf("beginnings of self-hosting"));
+  let int file = fopen(str_buf(filename), str_buf("r"));
+  check (file != 0);
+  log "opened file";
+  log file;
+  auto i = 0;
+  while (i < 100) {
+    auto ch = fgetc(file);
+    log ch;
+    i = i + 1;
   }
-  main {
-    filename = "comp/rustc.rc";
-    puts(str_buf("beginnings of self-hosting"));
-    let int file = fopen(str_buf(filename), str_buf("r"));
-    check (file != 0);
-    log "opened file";
-    log file;
-    auto i = 0;
-    while (i < 100) {
-      auto ch = fgetc(file);
-      log ch;
-      i = i + 1;
-    }
-    fclose(file);
-  }
+  fclose(file);
 }
