008aed3677d74fa58f5ab9283644603d8ffc92ae
Dave Herman <dherman@mozilla.com>
2010-01-12 09:13:59 -0800

    - a bit more over-abstraction in ALU logic-- will eventually re-lower (sorry) - another test case for ADD/SUB of 8-bit values

--- src/boot/be/x86.ml
+++ src/boot/be/x86.ml
@@ -1072,11 +1072,12 @@ let select_insn_misc (q:Il.quad') : Asm.frag =
 
 type alu_binop_codes =
      {
-       immslash: int;    (* FIXME: document this *)
-       rm_dst_op8: int;
-       rm_dst_op32: int;
-       rm_src_op8: int;
-       rm_src_op32: int;
+       insn: string;
+       immslash: int;    (* mod/rm "slash" code for imm-src variant *)
+       rm_dst_op8: int;  (* opcode for 8-bit r/m dst variant *)
+       rm_dst_op32: int; (* opcode for 32-bit r/m dst variant *)
+       rm_src_op8: int;  (* opcode for 8-bit r/m src variant *)
+       rm_src_op32: int; (* opcode for 32-bit r/m src variant *)
      }
 ;;
 
@@ -1085,14 +1086,14 @@ let alu_binop
     : Asm.frag =
   match (dst, src) with
       (Il.Reg ((Il.Hreg r), dst_ty), Il.Cell c)
-        when is_ty32 dst_ty && is_rm32 c ->
-          insn_rm_r codes.rm_src_op32 c (reg r)
+        when (is_ty32 dst_ty && is_rm32 c) || (is_ty8 dst_ty && is_rm8 c)
+          -> insn_rm_r codes.rm_src_op32 c (reg r)
 
     | (_, Il.Cell (Il.Reg ((Il.Hreg r), src_ty)))
-        when is_rm32 dst && is_ty32 src_ty ->
-        insn_rm_r codes.rm_dst_op32 dst (reg r)
+        when (is_rm32 dst && is_ty32 src_ty) || (is_rm8 dst && is_ty8 src_ty)
+          -> insn_rm_r codes.rm_dst_op32 dst (reg r)
 
-    | (_, Il.Imm (i, _)) when is_rm32 dst
+    | (_, Il.Imm (i, _)) when is_rm32 dst || is_rm8 dst
         -> insn_rm_r_imm_s8_s32 0x83 0x81 dst codes.immslash i
 
     | _ -> raise Unrecognized
@@ -1141,22 +1142,26 @@ let select_insn (q:Il.quad) : Asm.frag =
             let binop = alu_binop b.Il.binary_dst b.Il.binary_rhs in
             let mulop = mul_like b.Il.binary_rhs in
               match (b.Il.binary_dst, b.Il.binary_op) with
-                  (_, Il.ADD) -> binop { immslash=slash0;
+                  (_, Il.ADD) -> binop { insn="ADD";
+                                         immslash=slash0;
                                          rm_dst_op8=0x0;
                                          rm_dst_op32=0x1;
                                          rm_src_op8=0x2;
                                          rm_src_op32=0x3; }
-                | (_, Il.SUB) -> binop { immslash=slash5;
+                | (_, Il.SUB) -> binop { insn="SUB";
+                                         immslash=slash5;
                                          rm_dst_op8=0x28;
                                          rm_dst_op32=0x29;
                                          rm_src_op8=0x2a;
                                          rm_src_op32=0x2b; }
-                | (_, Il.AND) -> binop { immslash=slash4;
+                | (_, Il.AND) -> binop { insn="AND";
+                                         immslash=slash4;
                                          rm_dst_op8=0x20;
                                          rm_dst_op32=0x21;
                                          rm_src_op8=0x22;
                                          rm_src_op32=0x23; }
-                | (_, Il.OR) -> binop { immslash=slash1;
+                | (_, Il.OR) -> binop { insn="OR";
+                                        immslash=slash1;
                                         rm_dst_op8=0x08;
                                         rm_dst_op32=0x09;
                                         rm_src_op8=0x0a;
--- src/test/run-pass/u8-incr.rs
+++ src/test/run-pass/u8-incr.rs
@@ -2,9 +2,10 @@
 
 fn main() -> () {
   let u8 x = u8(12);
-  //let u8 y = u8(12);
+  let u8 y = u8(12);
   x = x + u8(1);
-  check(x == x);
+  x = x - u8(1);
+  check(x == y);
   //x = u8(14);
   //x = x + u8(1);
 }
