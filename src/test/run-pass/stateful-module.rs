77e2bd90c81e71371217c9225b9b7473c96aad69
Graydon Hoare <graydon@mozilla.com>
2010-01-29 11:43:55 -0500

    Add failing test for stateful modules.

---
+++ src/test/run-pass/stateful-module.rs
@@ -0,0 +1,19 @@
+
+// -*- C++ -*-
+
+mod counter(int x) {
+  fn incr() -> () {
+    x = x + 1;
+  }
+  fn get() -> int {
+    ret x;
+  }
+}
+
+fn main() -> () {
+  auto y = counter(0);
+  log y.get();
+  y.incr();
+  y.incr();
+  log y.get();
+}
