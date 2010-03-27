ecc9130117e254cef20a8f1a95cab7a9d4d089d8
Graydon Hoare <graydon@mozilla.com>
2010-03-26 17:17:50 -0700

    Test parametric function with multi-word type.

--- src/test/run-pass/generic-fn.rs
+++ src/test/run-pass/generic-fn.rs
@@ -4,11 +4,16 @@ fn id[T](T x) -> T {
    ret x;
 }
 
+type triple = (int,int,int);
+
 fn main() {
    auto x = 62;
    auto y = 63;
    auto a = 'a';
    auto b = 'b';
+
+   let triple p = (65, 66, 67);
+   let triple q = (68, 69, 70);
 
    y = id[int](x);
    log y;
@@ -17,5 +22,11 @@ fn main() {
    b = id[char](a);
    log b;
    check (a == b);
+
+   q = id[triple](p);
+   x = p._2;
+   y = q._2;
+   log y;
+   check (x == y);
 
 }
