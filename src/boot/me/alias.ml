7cc6f68e2d538d46ee4a98857f2915c34e0bf652
Dave Herman <dherman@mozilla.com>
2010-04-21 15:19:12 -0700

    inform alias analysis about foreach loop variable

--- src/boot/me/alias.ml
+++ src/boot/me/alias.ml
@@ -10,16 +10,19 @@ let alias_analysis_visitor
     (cx:ctxt)
     (inner:Walk.visitor)
     : Walk.visitor =
+  let alias_slot (slot_id:node_id) : unit =
+    begin
+      log cx "noting slot #%d as aliased" (int_of_node slot_id);
+      Hashtbl.replace cx.ctxt_slot_aliased slot_id ()
+    end
+  in
+
   let alias lval =
     match lval with
         Ast.LVAL_base nb ->
           let referent = Hashtbl.find cx.ctxt_lval_to_referent nb.id in
             if (referent_is_slot cx referent)
-            then
-              begin
-                log cx "noting slot #%d as aliased" (int_of_node referent);
-                Hashtbl.replace cx.ctxt_slot_aliased referent ()
-              end
+            then alias_slot referent
       | _ -> err None "unhandled form of lval in alias analysis"
   in
   let visit_stmt_pre s =
@@ -41,6 +44,9 @@ let alias_analysis_visitor
         | Ast.STMT_init_chan (dst, _) -> alias dst
         | Ast.STMT_init_vec (dst, _, _) -> alias dst
         | Ast.STMT_init_str (dst, _) -> alias dst
+        | Ast.STMT_foreach sfe ->
+            let (slot, _) = sfe.Ast.foreach_slot in
+              alias_slot slot.id
         | _ -> () (* FIXME (bug 541572): plenty more to handle here. *)
     end;
     inner.Walk.visit_stmt_pre s
