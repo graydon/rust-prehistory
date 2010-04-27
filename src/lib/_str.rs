8c389f8740a6672e2f6d9913cb824b72d30bb305
Graydon Hoare <graydon@mozilla.com>
2010-04-27 00:01:27 -0700

    Some mucking around with standard library.

--- src/lib/_str.rs
+++ src/lib/_str.rs
@@ -1,5 +1,10 @@
 native "rust" mod rustrt {
+  type sbuf;
   fn str_alloc(int n_bytes) -> str;
+  fn str_buf(str s) -> sbuf;
+}
+
+fn is_utf8(vec[u8] v) -> bool {
 }
 
 fn alloc(int n_bytes) -> str {
