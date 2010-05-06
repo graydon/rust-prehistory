f470d8af1621c335affbcb24cb1ebd9b17b7879c
Graydon Hoare <graydon@mozilla.com>
2010-05-06 16:43:53 -0700

    Add preliminary version of _vec.init. Doesn't seem to work yet.

--- src/lib/_vec.rs
+++ src/lib/_vec.rs
@@ -11,6 +11,16 @@ fn alloc[T](int n_elts) -> vec[T] {
   ret rustrt.vec_alloc[T](n_elts);
 }
 
+fn init[T](~T t, int n_elts) -> vec[T] {
+  let vec[T] v = alloc[T](n_elts);
+  let int i = n_elts;
+  while (i > 0) {
+    i -= 1;
+    v += vec(t);
+  }
+  ret v;
+}
+
 fn len[T](vec[T] v) -> uint {
   ret rustrt.vec_len[T](v);
 }
