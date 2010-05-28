60b0b2dab8d899f390dbefbb8607b23b11053176
Graydon Hoare <graydon@mozilla.com>
2010-05-27 17:06:01 -0700

    Enrich the generic-obj.rs test a bit.

--- src/test/run-pass/generic-obj.rs
+++ src/test/run-pass/generic-obj.rs
@@ -1,6 +1,13 @@
 obj buf[T](vec[T] data) {
+  fn get(int i) -> T {
+    ret data.(i);
+  }
 }
 
 fn main() {
-  let buf[int] b = buf[int](vec[int]());
+  let vec[int] v = vec(1,2,3);
+  let buf[int] b = buf[int](v);
+  log b.get(0);
+  log b.get(1);
+  log b.get(2);
 }
