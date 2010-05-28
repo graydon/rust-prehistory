1992e4ec06b6ffefebe0a1092feb50de5263414d
Graydon Hoare <graydon@mozilla.com>
2010-05-27 22:44:15 -0700

    Rewrite generic-obj.rs as something that doesn't require vec or obj dtors yet.

--- src/test/run-pass/generic-obj.rs
+++ src/test/run-pass/generic-obj.rs
@@ -1,12 +1,19 @@
-obj buf[T](vec[T] data) {
+obj buf[T]((T,T,T) data) {
   fn get(int i) -> T {
-    ret data.(i);
+    if (i == 0) {
+      ret data._0;
+    } else {
+      if (i == 1) {
+        ret data._1;
+      } else {
+        ret data._2;
+      }
+    }
   }
 }
 
 fn main() {
-  let vec[int] v = vec(1,2,3);
-  let buf[int] b = buf[int](v);
+  let buf[int] b = buf[int]((1,2,3));
   log b.get(0);
   log b.get(1);
   log b.get(2);
