238fb1192201233117db630d01c9a2b09bff94a8
Graydon Hoare <graydon@mozilla.com>
2010-06-12 14:37:36 -0700

    Simplify foreach-simple.rs test to not even access the put-value.

--- src/test/run-pass/foreach-simple.rs
+++ src/test/run-pass/foreach-simple.rs
@@ -3,12 +3,11 @@
 fn main() {
   for each (int i = first_ten()) {
     log "main";
-    log i;
   }
 }
 
 iter first_ten() -> int {
-  let int i = 97;
+  let int i = 90;
   while (i < 100) {
     log "first_ten";
     log i;
