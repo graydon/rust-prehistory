fd714f9a9361fd0c84bdb70510d8efbb326479d6
Graydon Hoare <graydon@mozilla.com>
2010-06-23 15:31:45 -0700

    Make rustc stub do even less, but use more of the std lib to do so.

--- src/comp/driver/rustc.rs
+++ src/comp/driver/rustc.rs
@@ -1,17 +1,12 @@
 // -*- C -*-
 
-fn main() -> () {
-  let str filename = "comp/rustc.rc";
-  libc.puts(librust.str_buf("beginnings of self-hosting"));
-  let int file = libc.fopen(librust.str_buf(filename), librust.str_buf("r"));
-  check (file != 0);
-  log "opened file";
-  log file;
-  auto i = 0;
-  while (i < 100) {
-    auto ch = libc.fgetc(file);
-    log ch;
-    i = i + 1;
+fn main(vec[str] args) -> () {
+  let int i = 0;
+  for (str filename in args) {
+    if (i > 0) {
+      auto br = std._io.mk_buf_reader(filename);
+      log "opened file: " + filename;
+    }
+    i += 1;
   }
-  libc.fclose(file);
 }
